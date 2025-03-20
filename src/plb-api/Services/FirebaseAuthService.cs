2d0ba08738392b3c5d074bc224d7dad7e8bfddf4
Nick Perry <nicholasjperry1@gmail.com>
2025-03-20 02:12:29 -0400

    creating FirebaseAuthService first pass

---
+++ src/plb-api/Services/FirebaseAuthService.cs
@@ -0,0 +1,25 @@
+﻿using FirebaseAdmin;
+using FirebaseAdmin.Auth;
+using Google.Apis.Auth.OAuth2;
+
+namespace plb_api.Services
+
+{
+    public class FirebaseAuthService
+    {
+        private readonly FirebaseAuth _firebaseAuth;
+
+        public FirebaseAuthService()
+        {
+            if (FirebaseApp.DefaultInstance == null)
+            {
+                FirebaseApp.Create(new AppOptions
+                {
+                    Credential = GoogleCredential.GetApplicationDefault()
+                });
+            }
+
+            _firebaseAuth = FirebaseAuth.DefaultInstance;
+        }
+    }
+}
